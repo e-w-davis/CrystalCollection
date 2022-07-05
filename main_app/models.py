eb0b6549b5b5ed2387f70a140b7b4def28571254
Eric W. Davis <e.w.davis15@gmail.com>
2022-07-05 17:40:37 -0400

    Working off of database info

--- main_app/models.py
+++ main_app/models.py
@@ -1,1 +1,12 @@
+from operator import mod
+from unicodedata import name
 from django.db import models
+
+class Crystal(models.Model):
+    name = models.CharField(max_length=100)
+    family = models.CharField(max_length=100)
+    hardness = models.IntegerField()
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Crystal
 
 def home(request):
     return render(request, 'home.html')
@@ -6,20 +7,6 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-class Crystal:
-    def __init__(self, name, family, hardness, color):
-        self.name = name
-        self.family = family
-        self.hardness = hardness
-        self.color = color
-
-crystals = [
-    Crystal('Amethyst', 'Quartz', 7, 'Purple'),
-    Crystal('Ruby', 'Beryl', 9, 'Red'),
-    Crystal('Diamond', 'Carbon-silicon', 10, 'Clear'),
-    Crystal('Labradorite', 'Feldspar', 6, 'Multicolored'),
-    Crystal('Fire Opal', 'Silicates Quartz', 6, 'Orange'),
-]
-
 def crystals_index(request):
+    crystals = Crystal.objects.all()
     return render(request, 'crystals/index.html', {'crystals': crystals})
