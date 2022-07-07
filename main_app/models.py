f1bda4ce95b21ab53d70066b4f53a1b45a7bc5dc
Eric W. Davis <e.w.davis15@gmail.com>
2022-07-07 13:42:11 -0400

    CRUD added to crystals and cleansing model migrated

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,15 @@
-from operator import mod
-from unicodedata import name
+from statistics import mode
 from django.db import models
+from django.urls import reverse
+
+METHODS = (
+    ('S', 'Smudging'),
+    ('W', 'Running Water'),
+    ('M', 'Full Moon'),
+    ('O', 'Other')
+)
+class Location(models.Model):
+    place = models.CharField(max_length=20)
 
 class Crystal(models.Model):
     name = models.CharField(max_length=100)
@@ -10,3 +19,18 @@ class Crystal(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'crystal_id': self.id})
+
+class Cleansing(models.Model):
+    date = models.DateField()
+    method = models.CharField(
+        max_length=1,
+        choices=METHODS,
+        default=METHODS[0][0]
+    )
+    crystal = models.ForeignKey(Crystal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_method_display()} on {self.date}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Crystal
 
 def home(request):
@@ -10,3 +11,22 @@ def about(request):
 def crystals_index(request):
     crystals = Crystal.objects.all()
     return render(request, 'crystals/index.html', {'crystals': crystals})
+
+def crystals_detail(request, crystal_id):
+    crystal = Crystal.objects.get(id=crystal_id)
+    return render(request, 'crystals/detail.html', {
+        'crystal': crystal,
+    })
+
+class CrystalCreate(CreateView):
+    model = Crystal
+    fields = '__all__'
+    success_url = '/crystals/'
+
+class CrystalUpdate(UpdateView):
+    model = Crystal
+    fields = ['family', 'hardness', 'color']
+
+class CrystalDelete(DeleteView):
+    model = Crystal
+    success_url = '/crystals/'
